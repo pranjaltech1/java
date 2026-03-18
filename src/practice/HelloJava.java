903b8bbc3b6242ccd78b11fb5430a57a382c26cb
avni <pranjal.pagaravi23@gmail.com>
2026-03-18 18:20:37 +0530

    map

--- src/practice/HelloJava.java
+++ src/practice/HelloJava.java
@@ -1,8 +1,47 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class HelloJava  {
     public static void main(String[] args) {
-    BankAccount sbi = new BankAccount();        // one shared account
+
+
+        HashMap<Integer,String> map=new HashMap<>();
+        map.put(1,"viha");
+        map.put(2,"sham");
+        map.put(3,"avni");
+        map.containsKey(2);
+        System.out.println(map);
+
+
+
+
+
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(10);
+        list.add(60);
+        list.add(40);
+
+        //comparator
+        System.out.println(list.get(0));
+        list.sort((a, b)->a-b);
+        System.out.println(list);
+        list.sort((a,b)->b-a);
+        System.out.println(list);
+
+
+
+
+
+
+
+
+
+
+
+
+   /* BankAccount sbi = new BankAccount();        // one shared account
 
     Runnable task = new Runnable() {
         @Override
@@ -16,5 +55,9 @@ public class HelloJava  {
 
         t1.start();
         t2.start();
+*/
+
+
+
 }
 }
---
+++ src/practice/arrayList.java
@@ -0,0 +1,12 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class arrayList {
+    List<String > list=new ArrayList<>();
+
+
+
+
+}
